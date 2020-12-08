e63f8f975f218fabeba0b2c70231e0f4c4fddd9d
Jiří Zdvomka <zdvomka.j@gmail.com>
2020-12-08 18:50:36 +0100

    Add packages

--- Package.swift
+++ Package.swift
@@ -1,20 +1,20 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SimulatedAnnealing",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(name: "Files", url: "https://github.com/johnsundell/files.git", from: "4.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SimulatedAnnealing",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "Files"
+            ]),
         .testTarget(
             name: "SimulatedAnnealingTests",
             dependencies: ["SimulatedAnnealing"]),
