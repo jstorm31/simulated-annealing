8aae303d00f3cc5a3bb962e0155a3899c2d10d08
Jiří Zdvomka <zdvomka.j@gmail.com>
2020-12-08 18:35:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simulated annealing
+
+This is a homework project for NI-KOP course on FIT CTU.
+
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SimulatedAnnealingTests
+
+var tests = [XCTestCaseEntry]()
+tests += SimulatedAnnealingTests.allTests()
+XCTMain(tests)
