6a4ab64e3ecc7b5bff7666f07d6417c13568bdcd
Jiří Zdvomka <zdvomka.j@gmail.com>
2020-12-27 13:17:06 +0100

    Add loading multiple SAT problems

--- Sources/SimulatedAnnealing/Engine/MWSATEngine.swift
+++ Sources/SimulatedAnnealing/Engine/MWSATEngine.swift
@@ -11,15 +11,21 @@ final class MWSATEngine: Engine {
     var problems = [(MWSATProblem, MWSATConfiguration?)]()
     
     func loadProblems(_ path: String, _ count: Int, _ solutionPath: String?) throws {
-        let problem = try MWSATProblem.loadProblem(path: path)
-        var solution: MWSATConfiguration?
-//        var directoryPath = path.split(separator: "/")
-//        directoryPath = Array(directoryPath[0...directoryPath.index(directoryPath.endIndex, offsetBy: -2)])
+        var directoryParts = NSString(string: path).expandingTildeInPath.split(separator: "/")
+        directoryParts = Array(directoryParts[0...directoryParts.index(directoryParts.endIndex, offsetBy: -2)])
+        let directoryPath = "/" + directoryParts.joined(separator: "/")
+        let problemPaths = try FileManager.default.contentsOfDirectory(atPath: directoryPath)
         
-        if let solutionPath = solutionPath {
-            solution = try MWSATProblem.loadSolution(solutionPath, problem)
+        for i in 0..<count {
+            let problemPath = directoryPath + "/" + problemPaths[i]
+            let problem = try MWSATProblem.loadProblem(path: problemPath)
+            var solution: MWSATConfiguration?
+            
+            if let solutionPath = solutionPath {
+                solution = try MWSATProblem.loadSolution(solutionPath, problem)
+            }
+            problems.append((problem, solution))
         }
-        problems.append((problem, solution))
     }
     
     func measure(plot: Bool) -> [SolverResult] {
